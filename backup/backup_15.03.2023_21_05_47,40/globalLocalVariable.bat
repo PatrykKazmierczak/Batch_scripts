da2c5a61e143d6429d02015f12ab8905dcf1e245
PatrykKazmierczak <patrykkazmierczak0@gmail.com>
2023-03-15 21:38:57 +0100

    update

--- backup/backup_15.03.2023_21_05_47,40/globalLocalVariable.bat
+++
@@ -1,8 +0,0 @@
-@echo off 
-set globalvar = 5
-SETLOCAL
-set var = 13145
-set /A var = %var% + 5
-echo %var%
-echo %globalvar%
-ENDLOCAL
--- backup/backup_15.03.2023_21_05_47,40/listingFilesDirectory.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo ============================ Process start ===========================================================================================
-echo ======= This is for listing down all the files in the directory Program files ========================================================
-dir "C:\scripts\scripts" > C:\scripts\scripts\lists.txt
-echo ============================ Process finish ===========================================================================================
