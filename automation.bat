2d402edd9c9c45943b91c93f336c4bd386f107af
PatrykKazmierczak <patrykkazmierczak0@gmail.com>
2023-03-19 18:30:25 +0100

    update

--- automation.bat
+++ automation.bat
@@ -2,22 +2,22 @@
 
 setlocal
 
-echo ============================ Process start ====================================================================
+echo ============================ Process start ======================================================================
 
-echo ============= Define the folder to store the extracted repos information ==========================================
+echo ============= Define the folder to store the extracted repos information ========================================
 
 set repos_info=C:\scripts\repos_info
-echo =============== Create the info folder if it doesn't exist ====================================================
+echo =============== Create the info folder if it doesn't exist ======================================================
 
 if not exist "%repos_info%" mkdir "%repos_info%"
-echo ============ Define the folder to store the extracted structure information ======================================
+echo ============ Define the folder to store the extracted structure information =====================================
 
 set storage_info=C:\scripts\storage_info
-echo =============== Create the info folder if it doesn't exist ====================================================
+echo =============== Create the info folder if it doesn't exist ======================================================
 
 if not exist "%storage_info%" mkdir "%storage_info%"
 
-echo =============== Searching for Git repositories in %USERPROFILE% ===============================================
+echo =============== Searching for Git repositories in %USERPROFILE% =================================================
 
 set "output_file=C:\scripts\repos_info\git_repositories.txt"
 
@@ -29,26 +29,26 @@ for /f "delims=" %%r in ('dir /b /s "%USERPROFILE%\*.git"') do (
   set "repo_dir=%%~dr%%~pr"
   cd /d "%%~dpnr"
 
-echo ========================= Check git status ======================================================================
+echo ========================= Check git status =======================================================================
   git status >> "%output_file%"
 
-echo ====================== Get current git branch ===================================================================
+echo ====================== Get current git branch ====================================================================
   git branch -r >> "%output_file%"
 
-echo ======================= Checkout main branch ====================================================================
+echo ======================= Checkout main branch =====================================================================
   git checkout main
 
-echo ===================== Fetch latest git changes ==================================================================
+echo ===================== Fetch latest git changes ===================================================================
   git fetch
 
-echo ===================== Pull latest git changes ===================================================================
+echo ===================== Pull latest git changes ====================================================================
   git pull
 
-echo ======================== Zip the repository =====================================================================
+echo ======================== Zip the repository ======================================================================
   set "zip_name=%%~nxr.zip"
   7z a -tzip "%USERPROFILE%\Downloads\%zip_name%" "*"
 
-echo ================ Move the repository to specific location========================================================
+echo ================ Move the repository to specific location=========================================================
   move /Y "%%r" "C:\scripts\storage_info\"
 
   cd /d "%USERPROFILE%"
