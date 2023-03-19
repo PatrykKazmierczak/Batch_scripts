91610b7a6583a4d71e207180a2a294f9a8133903
PatrykKazmierczak <patrykkazmierczak0@gmail.com>
2023-03-19 21:22:55 +0100

    update

--- automation.bat
+++ automation.bat
@@ -2,22 +2,25 @@
 
 setlocal
 
-echo ============================ Process start ======================================================================
+echo ================================== Process start =========================================================================
 
-echo ============= Define the folder to store the extracted repos information ========================================
+echo ================ Define the folder to store the extracted repos information ==============================================
 
 set repos_info=C:\scripts\repos_info
-echo =============== Create the info folder if it doesn't exist ======================================================
+
+echo ==================== Create the info folder if it doesn't exist ==========================================================
 
 if not exist "%repos_info%" mkdir "%repos_info%"
-echo ============ Define the folder to store the extracted structure information =====================================
+
+echo ============= Define the folder to store the extracted structure information =============================================
 
 set storage_info=C:\scripts\storage_info
-echo =============== Create the info folder if it doesn't exist ======================================================
+
+echo ====================== Create the info folder if it doesn't exist ========================================================
 
 if not exist "%storage_info%" mkdir "%storage_info%"
 
-echo =============== Searching for Git repositories in C:\ ===========================================================
+echo ======================== Searching for Git repositories in C:\ ===========================================================
 
 set "output_file=C:\scripts\repos_info\git_repositories.txt"
 
@@ -27,10 +30,13 @@ for /d /r "C:\" %%a in (*.git) do (
     git status > nul 2>&1
     if %errorlevel% EQU 0 (
         REM Repo exists
-        REM Log remote branches
+		
+echo ============================== Log remote branches =======================================================================
+
         git branch -r > "%repos_info%\remote_branches.txt"
     
-        REM Checkout main branch
+echo ============================== Checkout main branch ======================================================================
+
         git checkout main > nul 2>&1
         if %errorlevel% EQU 1 (
             REM Main branch doesn't exist, fall back to default branch specified in repos.info file
@@ -38,18 +44,21 @@ for /d /r "C:\" %%a in (*.git) do (
             git checkout %default_branch% > nul 2>&1
         )
     
-        REM Fetch and pull changes
+echo ============================= Fetch and pull changes =====================================================================
+
         git fetch > nul 2>&1
         git pull > nul 2>&1
     
-        REM Zip repo and move to storage folder
+echo ====================== Zip repo and move to storage folder ===============================================================
+
         set zip_name=%%~na.zip
         set zip_location=%storage_info%\%zip_name%
         git archive -o "%zip_location%" HEAD
         echo Moving "%zip_location%" to "%storage_info%\"
         move "%zip_location%" "%storage_info%\" > nul 2>&1
     
-        REM Update log file
+echo =============================== Update log file ==========================================================================
+
         set /p title=<"%repos_info%\title.txt"
         set /p tags=<"%repos_info%\tags.txt"
         set /p url=<"%repos_info%\url.txt"
